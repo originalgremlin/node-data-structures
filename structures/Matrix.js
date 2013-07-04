75ec5f2c964c0ff35fa05c41da8020ff0ddff26e
Barry Shapira <barry.shapira@gmail.com>
2013-07-04 12:29:16 +0300

    Add some property descriptors.

--- structures/Matrix.js
+++ structures/Matrix.js
@@ -15,12 +15,49 @@ var Matrix = Class.extend({
         if (_.isArray(this.values)) this.set(values);
     },
 
+    /*** properties ***/
     isSameSize: function (other) {
         return (this.rows === other.rows) && (this.cols === other.cols);
     },
 
     isTransposeSameSize: function (other) {
         return (this.rows === other.cols) && (this.cols === other.rows);
+    },
+
+    isSquare: function () {
+        return (this.rows === this.cols);
+    },
+
+    isDiagonal: function () {
+        // short-circuit failure if not square
+        if (!this.isSquare())
+            return false;
+        // make sure all off-diagonal elements are 0
+        var m, n, elem;
+        for (m = 0; m < other.rows; m++) {
+            for (n = 0; n < other.cols; n++) {
+                elem = this.matrix[m][n];
+                if (m !== n && elem !== 0)
+                    return false;
+            }
+        }
+        return true;
+    },
+
+    isIdentity: function () {
+        // short-circuit failure if not square
+        if (!this.isSquare())
+            return false;
+        // make sure all off-diagonal elements are 0 and on-diagonal elements are 1
+        var m, n, elem;
+        for (m = 0; m < other.rows; m++) {
+            for (n = 0; n < other.cols; n++) {
+                elem = this.matrix[m][n];
+                if ((m !== n && elem !== 0) || (m === n && elem !== 1))
+                    return false;
+            }
+        }
+        return true;
     },
 
     isEqual: function (other) {
@@ -36,31 +73,46 @@ var Matrix = Class.extend({
         return true;
     },
 
+    /*** derived properties ***/
+    getTranspose: function () {
+        return this;
+    },
+
+    getDeterminant: function () {
+
+    },
+
+    getEigenvalues: function () {
+        return this;
+    },
+
+    /*** copy operations ***/
+    // TODO: does node have any concept of memcopy?
     set: function (values) {
-        // copy the values
+        // copy by value
         var m, n;
-        for (m = 0, mlen = values.length; m < mlen; m++)
-            for (n = 0, nlen = values[0].length; n < nlen; n++)
+        for (m = 0; m < this.rows; m++)
+            for (n = 0; n < this.cols; n++)
                 this.matrix[m][n] = values[m][n];
         return this;
     },
 
     // return a matrix which is our exact copy
-    // TODO: does node have any concept of memcopy?
     clone: function () {
         var rv = new Matrix(this.rows, this.cols),
             m, n;
-        for (m = 0; m < other.rows; m++)
-            for (n = 0; n < other.cols; n++)
+        for (m = 0; m < this.rows; m++)
+            for (n = 0; n < this.cols; n++)
                 rv.matrix[m][n] = this.matrix[m][n];
         return rv;
     },
 
+    /*** algebraic operations ***/
     // add two matrices
     add: function (other) {
         var m, n;
-        for (m = 0; m < other.rows; m++)
-            for (n = 0; n < other.cols; n++)
+        for (m = 0; m < this.rows; m++)
+            for (n = 0; n < this.cols; n++)
                 this.matrix[m][n] += other.matrix[m][n];
         return this;
     },
@@ -74,8 +126,8 @@ var Matrix = Class.extend({
     // add the same value to all elements
     shift: function (value) {
         var m, n;
-        for (m = 0; m < other.rows; m++)
-            for (n = 0; n < other.cols; n++)
+        for (m = 0; m < this.rows; m++)
+            for (n = 0; n < this.cols; n++)
                 this.matrix[m][n] += value;
         return this;
     },
@@ -83,31 +135,32 @@ var Matrix = Class.extend({
     // multiply all elements by the same value
     scale: function (value) {
         var m, n;
-        for (m = 0; m < other.rows; m++)
-            for (n = 0; n < other.cols; n++)
+        for (m = 0; m < this.rows; m++)
+            for (n = 0; n < this.cols; n++)
                 this.matrix[m][n] *= value;
         return this;
     },
 
+    /*** utility functions ***/
     // apply some transform to each element
     map: function (func, args) {
         var m, n;
-        for (m = 0; m < other.rows; m++)
-            for (n = 0; n < other.cols; n++)
+        args = args || [];
+        for (m = 0; m < this.rows; m++)
+            for (n = 0; n < this.cols; n++)
                 this.matrix[m][n] = func.apply(this, [this.matrix, m, n].concat(args));
         return this;
     },
 
-    // get matrix statistics
-    getTranspose: function () {
-        return this;
-    },
-
-    getDeterminant: function () {
-
-    },
-
-    getEigenvalues: function () {
+    // check every element against a predicate function
+    // return true is all pass the predicate, and false if any fail
+    // XXX: unfinished
+    test: function (func, args) {
+        var m, n;
+        args = args || [];
+        for (m = 0; m < other.rows; m++)
+            for (n = 0; n < other.cols; n++)
+                this.matrix[m][n] = func.apply(this, [this.matrix, m, n].concat(args));
         return this;
     }
 });
